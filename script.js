3a6082e20f9cd36826f3a815d86df3416d76415b
syedsohanahmed <sdsohan005@gamail.com>
2023-04-06 21:50:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// get elements by their IDs
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const meridian = document.getElementById('meridian');
+
+// function to update the clock every second
+function updateClock() {
+  const now = new Date();
+
+  // get hours, minutes, and seconds
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  // convert to 12-hour format
+  let mer = "AM";
+  if (hour > 12) {
+    hour -= 12;
+    mer = "PM";
+  }
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  // pad with leading zeros if needed
+  hour = hour.toString().padStart(2, "0");
+  minute = minute.toString().padStart(2, "0");
+  second = second.toString().padStart(2, "0");
+
+  // update the clock display
+  hours.innerHTML = hour;
+  minutes.innerHTML = minute;
+  seconds.innerHTML = second;
+  meridian.innerHTML = mer;
+}
+
+// call the updateClock function every second
+setInterval(updateClock, 1000);
+
+// call the updateClock function once to initialize the clock
+updateClock();
